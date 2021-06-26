97b6eb33c9074a95ff25f70dad8057d7fdd03146
Francisco Puma <francis_pancho12@hotmail.com>
2021-06-26 11:16:24 -0700

    fixed routes and some bugs

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const path = require("path");
+const fs = require("fs");
+const PORT = 3001;
 const app = express();
+//const routes = require('./routes')
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
