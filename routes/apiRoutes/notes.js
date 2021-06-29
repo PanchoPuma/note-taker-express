5f72fcdec0662c5571f36aaab75c2e1cba10d825
Francisco Puma <francis_pancho12@hotmail.com>
2021-06-28 21:16:56 -0700

    working on the notes js

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
-const indexRoutes = require('./index.js')
+const { saveNote } = require('../../public/assets/js/index')
 const { notes } = require('../../db/db');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    res.json(results);
+    if (results) {
+        res.json(results);
+    } else {
+        res.send(404);
+    }
   });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, 
@@ -15,7 +19,11 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     const notes = saveNote(notes);
-    res.json(notes);
+    if (notes) {
+        res.json(notes);
+    } else {
+        res.send(404);
+    }
   });
 
 // You'll need to find a way to give each note a unique id when it's saved 
