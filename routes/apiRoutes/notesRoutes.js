c0ccfbc6b9c787c38e42b71398d42edbfec6bcaa
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-08-24 00:38:26 -0700

    working on the API routes and Instructions

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
-const {notes} = require('../../db/db');
+const {newNote , deleteNote} = require('../routesInstructions/routesInstructions');
+
+const {notes} = require('../../db/db.json');
+
+module.exports = router;
